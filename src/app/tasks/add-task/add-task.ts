ffed6ac34df51367ee743f73a0ce0d0c0bcb8a28
bhavikvashi180498 <bhavikvashi180498@gmail.com>
2026-03-25 11:09:58 +0000

    Forms: Two way data binding

--- src/app/tasks/add-task/add-task.ts
+++ src/app/tasks/add-task/add-task.ts
@@ -1,14 +1,21 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.html',
   styleUrl: './add-task.css',
 })
 export class AddTask {
 
   cancel = output<void>();
+  //forms related properties
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDueDate = signal('');
+
 
   onCancel() {
     this.cancel.emit();
